71555a6a43f1aa8fe7f2240a442a4be56e947b55
Milica Petrović <milica95pe@gmail.com>
2025-04-01 13:10:42 +0200

    Save local changes before pull

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
-const cors = require('cors');
+const corsOptions = {
+  origin: 'https://modovatestudio.com',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+};
 const app = express();
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 
 // Rute
